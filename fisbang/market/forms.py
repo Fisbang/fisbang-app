d443e75a8498e3a05ea69cdc765f67e16b957aaf
Ricky Hariady <ricky.hariady@gmail.com>
2015-03-14 15:35:29 +0700

    apply new design

--- fisbang/market/forms.py
+++ fisbang/market/forms.py
@@ -1,17 +1,12 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField, SelectMultipleField
+from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import Required, Length, Email, Regexp, EqualTo
-
-from flask.ext.security.forms import RegisterFormMixin, UniqueEmailFormMixin, NewPasswordFormMixin, PasswordConfirmFormMixin
 
 class CreateProjectForm(Form):
 
-    project_category = SelectField('Project Category', coerce=int, choices=[])
-    name = StringField('Project Name')
-    skills = StringField('Skills')
-    description = StringField('Description')
-    project_budget = SelectField('Budget', coerce=int, choices=[])
-    submit = SubmitField('Create Projects')
-
-class CreateProjectWithRegister(CreateProjectForm, RegisterFormMixin, UniqueEmailFormMixin, NewPasswordFormMixin, PasswordConfirmFormMixin):
-    pass
+    name = StringField('Your Name')
+    email = StringField('Your Email')
+    description = TextAreaField('Tell us about your idea/project')
+    budget = StringField('Already has budget?')
+    # deadline = StringField('And deadline')
+    # submit = SubmitField('Register')
